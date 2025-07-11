7c9c226e17cb38816243a729fda8193c280da4ff
Benjamin Krause <benjamin.krause@outlawraspberry.de>
2025-07-11 15:56:06 +0200

    Squash initial db migration

--- supabase/migrations/20250706174752_initial.sql
+++ supabase/migrations/20250706174752_initial.sql
@@ -2,30 +2,24 @@ create type "public"."game-state" as enum ('playing', 'paused');
 
 create type "public"."task_type" as enum ('attack', 'attack_and_claim', 'repair', 'claim');
 
-create schema if not exists "game";
-
-create type "game"."game-state" as enum ('playing');
-
-create type "game"."test" as enum ('test');
-
-create table "game"."fraction" (
+create table "public"."fraction" (
     "id" uuid not null default gen_random_uuid(),
     "created_at" timestamp with time zone not null default now(),
     "name" text not null
 );
 
 
-alter table "game"."fraction" enable row level security;
+alter table "public"."fraction" enable row level security;
 
-create table "game"."game" (
+create table "public"."public" (
     "tick" bigint not null,
     "state" "game-state" not null default 'paused'::"game-state"
 );
 
 
-alter table "game"."game" enable row level security;
+alter table "public"."public" enable row level security;
 
-create table "game"."point" (
+create table "public"."point" (
     "id" uuid not null default gen_random_uuid(),
     "created_at" timestamp with time zone not null default now(),
     "max_health" smallint not null default '255'::smallint,
@@ -33,9 +27,9 @@ create table "game"."point" (
 );
 
 
-alter table "game"."point" enable row level security;
+alter table "public"."point" enable row level security;
 
-create table "game"."tick_point" (
+create table "public"."tick_point" (
     "created_at" timestamp with time zone not null default now(),
     "point_id" uuid not null,
     "tick" bigint not null,
@@ -44,9 +38,9 @@ create table "game"."tick_point" (
 );
 
 
-alter table "game"."tick_point" enable row level security;
+alter table "public"."tick_point" enable row level security;
 
-create table "game"."tick_task" (
+create table "public"."tick_task" (
     "created_at" timestamp with time zone not null default now(),
     "created_by" uuid not null default gen_random_uuid(),
     "tick" bigint not null,
@@ -55,93 +49,193 @@ create table "game"."tick_task" (
 );
 
 
-alter table "game"."tick_task" enable row level security;
+alter table "public"."tick_task" enable row level security;
 
-create table "game"."user" (
+create table "public"."user" (
     "id" uuid not null,
     "fraction" uuid not null,
     "name" text not null
 );
 
 
-alter table "game"."user" enable row level security;
+alter table "public"."user" enable row level security;
 
-CREATE UNIQUE INDEX fraction_name_key ON game.fraction USING btree (name);
+CREATE UNIQUE INDEX fraction_name_key ON public.fraction USING btree (name);
 
-CREATE UNIQUE INDEX fraction_pkey ON game.fraction USING btree (id);
+CREATE UNIQUE INDEX fraction_pkey ON public.fraction USING btree (id);
 
-CREATE UNIQUE INDEX point_name_key ON game.point USING btree (name);
+CREATE UNIQUE INDEX point_name_key ON public.point USING btree (name);
 
-CREATE UNIQUE INDEX point_pkey ON game.point USING btree (id);
+CREATE UNIQUE INDEX point_pkey ON public.point USING btree (id);
 
-CREATE UNIQUE INDEX tick_point_pkey ON game.tick_point USING btree (point_id, tick);
+CREATE UNIQUE INDEX tick_point_pkey ON public.tick_point USING btree (point_id, tick);
 
-CREATE UNIQUE INDEX tick_task_pkey ON game.tick_task USING btree (created_by, tick);
+CREATE UNIQUE INDEX tick_task_pkey ON public.tick_task USING btree (created_by, tick);
 
-CREATE UNIQUE INDEX user_pkey ON game."user" USING btree (id);
+CREATE UNIQUE INDEX user_pkey ON public."user" USING btree (id);
 
-alter table "game"."fraction" add constraint "fraction_pkey" PRIMARY KEY using index "fraction_pkey";
+alter table "public"."fraction" add constraint "fraction_pkey" PRIMARY KEY using index "fraction_pkey";
 
-alter table "game"."point" add constraint "point_pkey" PRIMARY KEY using index "point_pkey";
+alter table "public"."point" add constraint "point_pkey" PRIMARY KEY using index "point_pkey";
 
-alter table "game"."tick_point" add constraint "tick_point_pkey" PRIMARY KEY using index "tick_point_pkey";
+alter table "public"."tick_point" add constraint "tick_point_pkey" PRIMARY KEY using index "tick_point_pkey";
 
-alter table "game"."tick_task" add constraint "tick_task_pkey" PRIMARY KEY using index "tick_task_pkey";
+alter table "public"."tick_task" add constraint "tick_task_pkey" PRIMARY KEY using index "tick_task_pkey";
 
-alter table "game"."user" add constraint "user_pkey" PRIMARY KEY using index "user_pkey";
+alter table "public"."user" add constraint "user_pkey" PRIMARY KEY using index "user_pkey";
 
-alter table "game"."fraction" add constraint "fraction_name_key" UNIQUE using index "fraction_name_key";
+alter table "public"."fraction" add constraint "fraction_name_key" UNIQUE using index "fraction_name_key";
 
-alter table "game"."point" add constraint "point_name_key" UNIQUE using index "point_name_key";
+alter table "public"."point" add constraint "point_name_key" UNIQUE using index "point_name_key";
 
-alter table "game"."tick_point" add constraint "tick_point_acquired_by_fkey" FOREIGN KEY (acquired_by) REFERENCES game.fraction(id) ON UPDATE CASCADE ON DELETE RESTRICT not valid;
+alter table "public"."tick_point" add constraint "tick_point_acquired_by_fkey" FOREIGN KEY (acquired_by) REFERENCES public.fraction(id) ON UPDATE CASCADE ON DELETE RESTRICT not valid;
 
-alter table "game"."tick_point" validate constraint "tick_point_acquired_by_fkey";
+alter table "public"."tick_point" validate constraint "tick_point_acquired_by_fkey";
 
-alter table "game"."tick_point" add constraint "tick_point_point_id_fkey" FOREIGN KEY (point_id) REFERENCES game.point(id) not valid;
+alter table "public"."tick_point" add constraint "tick_point_point_id_fkey" FOREIGN KEY (point_id) REFERENCES public.point(id) not valid;
 
-alter table "game"."tick_point" validate constraint "tick_point_point_id_fkey";
+alter table "public"."tick_point" validate constraint "tick_point_point_id_fkey";
 
-alter table "game"."tick_task" add constraint "tick_task_created_by_fkey" FOREIGN KEY (created_by) REFERENCES auth.users(id) ON UPDATE CASCADE ON DELETE RESTRICT not valid;
+alter table "public"."tick_task" add constraint "tick_task_created_by_fkey" FOREIGN KEY (created_by) REFERENCES auth.users(id) ON UPDATE CASCADE ON DELETE RESTRICT not valid;
 
-alter table "game"."tick_task" validate constraint "tick_task_created_by_fkey";
+alter table "public"."tick_task" validate constraint "tick_task_created_by_fkey";
 
-alter table "game"."tick_task" add constraint "tick_task_point_fkey" FOREIGN KEY (point) REFERENCES game.point(id) ON UPDATE CASCADE ON DELETE RESTRICT not valid;
+alter table "public"."tick_task" add constraint "tick_task_point_fkey" FOREIGN KEY (point) REFERENCES public.point(id) ON UPDATE CASCADE ON DELETE RESTRICT not valid;
 
-alter table "game"."tick_task" validate constraint "tick_task_point_fkey";
+alter table "public"."tick_task" validate constraint "tick_task_point_fkey";
 
-alter table "game"."user" add constraint "user_fraction_fkey" FOREIGN KEY (fraction) REFERENCES game.fraction(id) ON UPDATE CASCADE ON DELETE RESTRICT not valid;
+alter table "public"."user" add constraint "user_fraction_fkey" FOREIGN KEY (fraction) REFERENCES public.fraction(id) ON UPDATE CASCADE ON DELETE RESTRICT not valid;
 
-alter table "game"."user" validate constraint "user_fraction_fkey";
+alter table "public"."user" validate constraint "user_fraction_fkey";
 
-alter table "game"."user" add constraint "user_id_fkey" FOREIGN KEY (id) REFERENCES auth.users(id) ON UPDATE CASCADE ON DELETE CASCADE not valid;
+alter table "public"."user" add constraint "user_id_fkey" FOREIGN KEY (id) REFERENCES auth.users(id) ON UPDATE CASCADE ON DELETE CASCADE not valid;
 
-alter table "game"."user" validate constraint "user_id_fkey";
+alter table "public"."user" validate constraint "user_id_fkey";
 
-GRANT USAGE ON SCHEMA game TO anon,
+-- For public.tick_point.acquired_by → public.fraction(id)
+COMMENT ON CONSTRAINT tick_point_acquired_by_fkey ON public.tick_point IS '@graphql({"local_name": "acquiredByFraction", "foreign_name": "acquiredTickPoints"})';
+
+-- For public.tick_point.point_id → public.point(id)
+COMMENT ON CONSTRAINT tick_point_point_id_fkey ON public.tick_point IS '@graphql({"local_name": "point", "foreign_name": "tickPoints"})';
+
+-- For public.tick_task.created_by → auth.users(id)
+COMMENT ON CONSTRAINT tick_task_created_by_fkey ON public.tick_task IS '@graphql({"local_name": "createdByUser", "foreign_name": "createdTickTasks"})';
+
+-- For public.tick_task.point → public.point(id)
+COMMENT ON CONSTRAINT tick_task_point_fkey ON public.tick_task IS '@graphql({"local_name": "point", "foreign_name": "tickTasks"})';
+
+-- For public.user.fraction → public.fraction(id)
+COMMENT ON CONSTRAINT user_fraction_fkey ON public."user" IS '@graphql({"local_name": "fraction", "foreign_name": "users"})';
+
+-- For public.user.id → auth.users(id)
+COMMENT ON CONSTRAINT user_id_fkey ON public."user" IS '@graphql({"local_name": "authUser", "foreign_name": "publicUsers"})';
+
+
+GRANT USAGE ON SCHEMA public TO anon,
 authenticated,
 service_role;
 
-GRANT ALL ON ALL TABLES IN SCHEMA game TO anon,
+GRANT ALL ON ALL TABLES IN SCHEMA public TO anon,
 authenticated,
 service_role;
 
-GRANT ALL ON ALL ROUTINES IN SCHEMA game TO anon,
+GRANT ALL ON ALL ROUTINES IN SCHEMA public TO anon,
 authenticated,
 service_role;
 
-GRANT ALL ON ALL SEQUENCES IN SCHEMA game TO anon,
+GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO anon,
 authenticated,
 service_role;
 
-ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA game GRANT ALL ON TABLES TO anon,
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public GRANT ALL ON TABLES TO anon,
 authenticated,
 service_role;
 
-ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA game GRANT ALL ON ROUTINES TO anon,
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public GRANT ALL ON ROUTINES TO anon,
 authenticated,
 service_role;
 
-ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA game GRANT ALL ON SEQUENCES TO anon,
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public GRANT ALL ON SEQUENCES TO anon,
 authenticated,
 service_role;
+
+alter table "public"."user" alter column "name" set default ''::text;
+
+CREATE UNIQUE INDEX user_name_key ON public."user" USING btree (name);
+
+alter table "public"."user" add constraint "user_name_check" CHECK ((length(name) < 50)) not valid;
+
+alter table "public"."user" validate constraint "user_name_check";
+
+alter table "public"."user" add constraint "user_name_key" UNIQUE using index "user_name_key";
+
+create policy "Enable read access for all users"
+on "public"."fraction"
+as permissive
+for select
+to public
+using (true);
+
+
+create policy "Enable read access for all users"
+on "public"."public"
+as permissive
+for select
+to public
+using (true);
+
+
+create policy "Enable read access for all users"
+on "public"."point"
+as permissive
+for select
+to public
+using (true);
+
+
+create policy "Enable read access for all users"
+on "public"."tick_point"
+as permissive
+for select
+to public
+using (true);
+
+
+create policy "Enable delete for users based on user_id"
+on "public"."tick_task"
+as permissive
+for delete
+to public
+using ((( SELECT auth.uid() AS uid) = created_by));
+
+
+create policy "Enable insert for users based on user_id"
+on "public"."tick_task"
+as permissive
+for insert
+to authenticated
+with check ((( SELECT auth.uid() AS uid) = created_by));
+
+
+create policy "Enable update for users based on user_id"
+on "public"."tick_task"
+as permissive
+for update
+to public
+using ((( SELECT auth.uid() AS uid) = created_by));
+
+
+create policy "Enable insert for users based on user_id"
+on "public"."user"
+as permissive
+for insert
+to authenticated
+with check ((( SELECT auth.uid() AS uid) = id));
+
+
+create policy "Enable users to view their own data only"
+on "public"."user"
+as permissive
+for select
+to authenticated
+using ((( SELECT auth.uid() AS uid) = id));
