f70186fdae4c3cab15b719c5504b50b04e24c765
Benjamin Krause <benjamin.krause@outlawraspberry.de>
2025-07-18 14:31:55 +0200

    Update cors headers

--- supabase/functions/cors.ts
+++ supabase/functions/cors.ts
@@ -1,5 +1,5 @@
-export const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
+export const corsHeaders= {
+  "Access-Control-Allow-Origin": Deno.env.get("CORS_ORIGIN") ?? "http://localhost:5173",
   "Access-Control-Allow-Headers":
     "authorization, x-client-info, apikey, content-type",
 };
