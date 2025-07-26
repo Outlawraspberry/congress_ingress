91ab95936350201f5797111ca6fa0854634c4ec7
Benjamin Krause <benjamin.krause@outlawraspberry.de>
2025-07-26 11:33:16 +0200

    Use progressbar to improve health indicator

--- game-client/src/routes/game/point/[pointId]/+page.svelte
+++ game-client/src/routes/game/point/[pointId]/+page.svelte
@@ -2,12 +2,18 @@
 	import TaskOverview from '$lib/components/task/task-overview.svelte';
 	import faction from '$lib/supabase/faction/faction';
 	import type { PointState } from '$lib/supabase/game/points.svelte';
-	import { Heading, P } from 'flowbite-svelte';
+	import { Heading, P, Progressbar } from 'flowbite-svelte';
 	import { onDestroy } from 'svelte';
 
 	const { data }: { data: { point: PointState } } = $props();
 
 	let factionName = $state('Unclaimed');
+
+	let progress = $derived(
+		data.point.state.point
+			? Math.round((100 / data.point.state.point.max_health) * data.point.state.point.health)
+			: 0
+	);
 
 	$effect(() => {
 		if (data.point.state.point?.acquired_by) {
@@ -26,13 +32,16 @@
 
 <Heading class="text-center" tag="h1">{data.point.state.point?.name}</Heading>
 
-<section class="my-5">
-	<P class="text-center">Acquired by: {factionName}</P>
-	<P class="text-center"
-		>Health: {data.point.state.point?.health} / {data.point.state.point?.max_health}</P
-	>
-</section>
+<section class="container">
+	<section class="my-5">
+		<P class="text-center">Acquired by: {factionName}</P>
+		<Progressbar {progress} animate={true} size="h-6"></Progressbar>
+		<P class="text-center">
+			{data.point.state.point?.health} / {data.point.state.point?.max_health}
+		</P>
+	</section>
 
-<section class="container my-5 flex justify-center">
-	<TaskOverview chosenPoint={data.point}></TaskOverview>
+	<section class="container my-5 flex justify-center">
+		<TaskOverview chosenPoint={data.point}></TaskOverview>
+	</section>
 </section>
