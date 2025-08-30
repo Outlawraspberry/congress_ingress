25f26dab46c1627f9dda8180f03f24b1a7b10ee4
Benjamin Krause <benjamin.krause@outlawraspberry.de>
2025-08-30 20:23:05 +0200

    Fix does_username_exists function to allow it to anon users

--- supabase/migrations/20250830173736_add_does_username_exists_function.sql
+++ supabase/migrations/20250830173736_add_does_username_exists_function.sql
@@ -3,9 +3,8 @@ set check_function_bodies = off;
 CREATE OR REPLACE FUNCTION public.does_username_exists(a_username text)
  RETURNS boolean
  LANGUAGE sql
+ SECURITY DEFINER
 AS $function$SELECT EXISTS (
   SELECT 1 FROM public.user WHERE public.user.name = a_username
 ) AS exists_bool;$function$
 ;
-
-
