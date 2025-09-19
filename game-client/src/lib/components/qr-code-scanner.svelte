3388d9983ffdab7c8fe87702755a98a512498672
Benjamin Krause <benjamin.krause@outlawraspberry.de>
2025-09-19 11:53:41 +0200

    Add switch camera button

--- game-client/src/lib/components/qr-code-scanner.svelte
+++ game-client/src/lib/components/qr-code-scanner.svelte
@@ -1,43 +1,46 @@
 <script lang="ts">
-	import { Button } from 'flowbite-svelte';
+	import { Alert, Button, P } from 'flowbite-svelte';
 	import { Section } from 'flowbite-svelte-blocks';
-	import { Html5Qrcode } from 'html5-qrcode';
+	import { Html5Qrcode, type CameraDevice } from 'html5-qrcode';
+	import { onMount } from 'svelte';
 
 	let { onTextFound: onFoundCode }: { onTextFound: (codeText: string) => void } = $props();
 
+	let isLoadingDevices = $state(true);
 	let isScanning = $state(false);
 
 	let html5QrCode: Html5Qrcode | null = $state(null);
-
 	let readerDiv: HTMLDivElement | null = $state(null);
+
+	let devices: CameraDevice[] = $state([]);
+	let currentDeviceIndex: number | null = null;
+
+	let currentDevice: CameraDevice | null = $derived(
+		devices.length > 0 && currentDeviceIndex !== null ? devices[currentDeviceIndex] : null
+	);
 
 	$effect(() => {
 		if (readerDiv != null) html5QrCode = new Html5Qrcode(/* element id */ 'reader');
 	});
 
-	// This method will trigger user permissions
-	function getCamera() {
-		return Html5Qrcode.getCameras().then((devices) => {
-			/**
-			 * devices would be an array of objects of type:
-			 * { id: "id", label: "label" }
-			 */
-			if (devices && devices.length) {
-				return devices[0].id;
-				// .. use this to start scanning.
-			}
-			throw new Error('no device found');
-		});
-	}
+	onMount(async () => {
+		devices = await Html5Qrcode.getCameras();
 
-	async function scanQr(cameraId: string) {
-		if (html5QrCode == null) return;
+		if (devices.length > 0) {
+			currentDeviceIndex = 0;
+			startScanning();
+		}
+
+		isLoadingDevices = false;
+	});
+
+	async function scanQr() {
+		if (html5QrCode == null || currentDeviceIndex === null) return;
 
 		html5QrCode.start(
-			cameraId,
+			devices[currentDeviceIndex].id,
 			{
-				fps: 10, // Optional, frame per seconds for qr code scanning
-				qrbox: { width: 400, height: 400 } // Optional, if you want bounded box UI
+				fps: 10 // Optional, frame per seconds for qr code scanning
 			},
 			(text) => {
 				onFoundCode(text);
@@ -48,23 +51,48 @@
 	}
 
 	async function startScanning() {
-		const cameraId = await getCamera();
-		await scanQr(cameraId);
-		isScanning = true;
+		if (currentDeviceIndex !== null) {
+			await scanQr();
+			isScanning = true;
+		}
 	}
 
 	async function stopScanning() {
-		await html5QrCode?.stop();
-		isScanning = false;
+		if (html5QrCode?.isScanning) {
+			await html5QrCode?.stop();
+			isScanning = false;
+		}
+	}
+
+	async function switchCamera(): Promise<void> {
+		if (currentDeviceIndex !== null) {
+			await stopScanning();
+			currentDeviceIndex = (currentDeviceIndex + 1) % devices.length;
+			startScanning();
+		}
 	}
 </script>
 
-<Section divClass="flex justify-center items-center"
-	>{#if isScanning}
+<Section divClass="flex justify-center items-center">
+	{#if isScanning}
 		<Button onclick={stopScanning}>Stop scanning</Button>
 	{:else}
 		<Button onclick={startScanning}>Start Scanning</Button>
 	{/if}
+
+	{#if currentDevice == null && isLoadingDevices === false}
+		<Alert color="red">
+			It seems, that you don't have a camera in your devices, or didn't allowed us to use your
+			camera..</Alert
+		>
+	{/if}
 </Section>
 
 <div bind:this={readerDiv} id="reader"></div>
+
+<Section divClass="flex justify-center items-center flex-wrap flex-col">
+	{#if isScanning && currentDevice}
+		<P>{currentDevice.label}</P>
+		<Button onclick={switchCamera}>Switch device</Button>
+	{/if}
+</Section>
