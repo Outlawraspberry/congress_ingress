51cdc0ecac3a8e609a6b338305ce53e741b9a669
Benjamin Krause <benjamin.krause@outlawraspberry.de>
2025-09-26 11:57:30 +0200

    Adjust header to show logout when not admin

--- game-client/src/lib/components/header.svelte
+++ game-client/src/lib/components/header.svelte
@@ -20,17 +20,20 @@
 				{#if user.user == null}
 					<li><a href="/login">Login</a></li>
 					<li><a href="/register">Register</a></li>
-				{:else if user.user.role === 'admin'}
-					<li>
-						<details>
-							<summary>Admin Lounge</summary>
-							<ul class="bg-base-100 rounded-t-none p-2">
-								<li><a href="/admin/">Overview</a></li>
-								<li><a href="/admin/game">Game</a></li>
-								<li><a href="/admin/poing">Points</a></li>
-							</ul>
-						</details>
-					</li>
+				{:else}
+					{#if user.user.role === 'admin'}
+						<li>
+							<details>
+								<summary>Admin Lounge</summary>
+								<ul class="bg-base-100 rounded-t-none p-2">
+									<li><a href="/admin/">Overview</a></li>
+									<li><a href="/admin/game">Game</a></li>
+									<li><a href="/admin/poing">Points</a></li>
+								</ul>
+							</details>
+						</li>
+					{/if}
+
 					<li><a href="/logout" data-sveltekit-preload-data="off">Logout</a></li>
 				{/if}
 			</ul>
