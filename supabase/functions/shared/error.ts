25c5e91415dfdd5c88ff2118e98d6b5f49d968a4
Benjamin Krause <benjamin.krause@outlawraspberry.de>
2025-10-10 14:50:10 +0200

    Simplify perform-action edge function with new db functions

--- supabase/functions/shared/error.ts
+++ supabase/functions/shared/error.ts
@@ -1,4 +1,4 @@
-import { ErrorResult } from "../../../types/error-code.ts";
+import { ErrorCode, ErrorResult } from "../../../types/error-code.ts";
 import { corsHeaders } from "../cors.ts";
 
 export function handleError(
@@ -16,4 +16,41 @@ export function handleError(
       headers: { ...corsHeaders, "Content-Type": "application/json" },
     },
   );
+}
+
+export function errorHandler(e: unknown): Response {
+  let content: string;
+  let status: number = 500;
+
+  if (typeof e !== "object" || e == null) {
+    return handleError({
+      errorCode: ErrorCode.INTERNAL_ERROR,
+      httpStatus: status,
+      message: "Unknown error",
+    });
+  }
+
+  if (
+    "status" in e &&
+    typeof e.status === "number"
+  ) {
+    status = e.status;
+  }
+
+  if (
+    "message" in e &&
+    typeof e.message === "string"
+  ) {
+    content = e.message;
+  } else if (e instanceof Error) {
+    content = e.message;
+  } else {
+    content = JSON.stringify(e);
+  }
+
+  return handleError({
+    errorCode: ErrorCode.INTERNAL_ERROR,
+    httpStatus: status,
+    message: content,
+  });
 }
