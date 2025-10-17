ea4b63f071d335b9bc3a4ec0a5fc69ae5bfea753
Benjamin Krause <benjamin.krause@outlawraspberry.de>
2025-10-17 11:05:48 +0200

    Fix incorrect destroying of point when solving a puzzle

---
+++ game-client/src/routes/game/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { destroySelectedPoint } from '$lib/point/selected-point.svelte';
+	import { onDestroy } from 'svelte';
+
+	const { children } = $props();
+
+	onDestroy(() => {
+		destroySelectedPoint();
+	});
+</script>
+
+{@render children()}
