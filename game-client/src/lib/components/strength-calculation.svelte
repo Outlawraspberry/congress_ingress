c5a85125a39167cc048a6f1a74fa565171efd09a
Benjamin Krause <benjamin.krause@outlawraspberry.de>
2025-10-26 14:45:49 +0100

    Show diagram of strength relativ to users

--- game-client/src/lib/components/strength-calculation.svelte
+++ game-client/src/lib/components/strength-calculation.svelte
@@ -29,6 +29,20 @@
 		userBaseDamage: user_base_damage,
 		userMaxDamage: user_max_damage
 	});
+
+	// Calculate attack strength for user counts 1-10
+	$: attackStrengths = Array.from({ length: 10 }, (_, i) => {
+		const users = i + 1;
+		return {
+			users,
+			strength: calculateStrength({
+				groupModifier: group_attack_multiplier_per_user,
+				numberOfUsersAtPoint: users,
+				userBaseDamage: user_base_damage,
+				userMaxDamage: user_max_damage
+			})
+		};
+	});
 </script>
 
 <div class="">
@@ -60,5 +74,25 @@
     {user_max_damage},
     {user_base_damage} + ({number_of_users_at_point} - 1) × {group_attack_multiplier_per_user}
   ) × {number_of_users_at_point}</pre>
+	</div>
+
+	<div class="my-8">
+		<h3 class="mb-2 font-bold">Attack Strength by Number of Users</h3>
+		<div class="flex flex-col gap-2">
+			{#each attackStrengths as { users, strength } (users)}
+				<div class="flex items-center gap-2">
+					<span class="w-8 text-right">{users}</span>
+					<div class="bg-base-200 relative h-5 flex-1 rounded">
+						<div
+							class="h-5 rounded bg-blue-500"
+							style="width: {Math.min(strength, user_max_damage * 1.25) /
+								((user_max_damage * 1.25) / 100)}%"
+							title="Attack Strength"
+						></div>
+						<span class="absolute top-0 left-2 text-xs text-white">{strength}</span>
+					</div>
+				</div>
+			{/each}
+		</div>
 	</div>
 </div>
