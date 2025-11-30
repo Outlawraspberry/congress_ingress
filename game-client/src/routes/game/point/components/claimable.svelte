b0626c2535981900085fe180fd087784d3eac245
Benjamin Krause <benjamin.krause@outlawraspberry.de>
2025-11-30 05:30:39 +0100

    Use collapsables for point introduction

--- game-client/src/routes/game/point/components/claimable.svelte
+++ game-client/src/routes/game/point/components/claimable.svelte
@@ -1,1 +1,7 @@
-<h1 class="text-2xl">Claimable Point</h1>
+<div class="bg-base-100 border-base-300 collapse border">
+	<input type="checkbox" />
+	<div class="collapse-title font-semibold">What are claimable points?</div>
+	<div class="collapse-content text-sm">
+		Click the "Sign Up" button in the top right corner and follow the registration process.
+	</div>
+</div>
--- game-client/src/routes/game/point/components/minigame.svelte
+++ game-client/src/routes/game/point/components/minigame.svelte
@@ -1,1 +1,14 @@
-<h1 class="text-2xl">Minigame Point</h1>
+<script>
+	import Dock from '$lib/components/dock/dock.svelte';
+</script>
+
+<div class="bg-base-100 border-base-300 collapse max-w-full border md:max-w-80">
+	<input type="checkbox" />
+	<div class="collapse-title font-semibold">What are mini game points?</div>
+	<div class="collapse-content text-sm">
+		At mini game points, you can earn action points by solving small games, aka. mini games. By
+		clicking the button <span class="font-mono">Solve puzzle</span>, a random puzzle will be
+		generated for you. With an equal chance, you will have to solve a math puzzle, turn off lights
+		or play other classic games.
+	</div>
+</div>
