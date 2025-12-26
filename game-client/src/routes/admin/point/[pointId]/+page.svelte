0c58bcc009856c4f00717757fc3ecbce981ea506
Benjamin Krause <benjamin.krause@outlawraspberry.de>
2025-12-27 00:13:19 +0100

    Rename QR code pdfs that they include the point's name

--- game-client/src/routes/admin/point/[pointId]/+page.svelte
+++ game-client/src/routes/admin/point/[pointId]/+page.svelte
@@ -24,7 +24,7 @@
 		const url = URL.createObjectURL(blob);
 		const a = document.createElement('a');
 		a.href = url;
-		a.download = `mapping_${mappingId}_to_point_${data.pointData.id}.pdf`;
+		a.download = `${data.pointData.name}.pdf`;
 		document.body.appendChild(a);
 		a.click();
 		a.remove();
@@ -49,11 +49,21 @@
 		</p>
 	</section>
 
-
 	<div class="mt-4 flex gap-2">
 		<a class="btn btn-primary btn-sm" href={`/admin/point/${data.pointData.id}/edit`}>
-			<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-4 w-4"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+				/>
 			</svg>
 			Edit
 		</a>
@@ -62,7 +72,11 @@
 			Generate QR Code
 		</button>
 
-		<a class="btn btn-sm" data-sveltekit-preload-data="off" href={`/game/point/${data.pointData.id}`}>
+		<a
+			class="btn btn-sm"
+			data-sveltekit-preload-data="off"
+			href={`/game/point/${data.pointData.id}`}
+		>
 			Visit
 		</a>
 	</div>
